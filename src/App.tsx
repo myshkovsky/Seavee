768da23dbc8947ecd3b80a82484260ae82c8c4a9
myshkovsky <84045642+myshkovsky@users.noreply.github.com>
2023-07-22 18:03:36 +0200

    init edu card

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,28 @@ import { useState } from 'react'
 import defaultUser from './utils/defaultUser'
 import Debug from './components/Debug'
 import './styles/App.css'
-import { Card, Form, Input } from 'antd'
+import { Button, Card, DatePicker, Form, Input, Select } from 'antd'
 
 function App() {
   const [user, setUser] = useState(defaultUser)
+  const [eduForm] = Form.useForm()
 
   function handleInfoChange(targetProp: string, newValue: string) {
     setUser({...user, info: { ...user.info, [targetProp]: newValue }})
   }
 
+  function eduFormSubmit(values: unknown) {
+    console.log(values)
+  }
+
+  //TODO: Use <ConfigProvider> to get rid of the same style for every form item
+
   return (
     <article>
       <Debug user={user}/>
-      <Card title="Information" style={{ maxWidth: 400, textAlign: 'center', border: '1px solid #858585' }}>
+      <Card title="Information" style={{ maxWidth: 400, textAlign: 'center', border: '1px solid #858585', marginBottom: '1em' }}>
         <Form
-          labelCol={{ span: 8 }}
+          labelCol={{ span: 5 }}
           layout='horizontal'
         >
           <Form.Item
@@ -45,7 +52,7 @@ function App() {
           </Form.Item>
           <Form.Item
             name="phone"
-            label='Phone number:'
+            label='Phone:'
             style={{ marginBottom: "1em" }}
             rules={[{ required: true, message: 'You must enter a phone number.' }]}
           >
@@ -58,6 +65,61 @@ function App() {
           >
             <Input defaultValue={user.info.location} onChange={e => handleInfoChange('location', e.target.value)}/>
           </Form.Item>
+        </Form>
+      </Card>
+      <Card title="Education" style={{ maxWidth: 400, textAlign: 'center', border: '1px solid #858585' }}>
+        <Form
+          labelCol={{ span: 5 }}
+          layout='horizontal'
+          form={eduForm}
+          onFinish={eduFormSubmit}
+          style={{ textAlign: 'center' }}
+        >
+          <Form.Item
+            name="school"
+            label='School:'
+            style={{ marginBottom: "1em" }}
+          >
+            <Input placeholder='Harvard University'/>
+          </Form.Item>
+          <Form.Item
+            name="major"
+            label='Major:'
+            style={{ marginBottom: "1em" }}
+          >
+            <Input placeholder='Computer Science'/>
+          </Form.Item>
+          <Form.Item
+            name="degree"
+            label='Degree:'
+            style={{ marginBottom: "1em" }}
+          >
+            <Select style={{ textAlign: 'left', marginBottom: "1em" }}>
+              <Select.Option value="associate">Associate</Select.Option>
+              <Select.Option value="bachelors">Bachelor's</Select.Option>
+              <Select.Option value="masters">Master's</Select.Option>
+              <Select.Option value="doctorate">Doctorate</Select.Option>
+            </Select>
+          </Form.Item>
+          <div style={{ textAlign: 'left' }}>
+            <Form.Item
+              name="start"
+              label='Start:'
+              style={{ marginBottom: "1em" }}
+            >
+              <DatePicker />
+            </Form.Item>
+            <Form.Item
+              name="end"
+              label='End:'
+              style={{ marginBottom: "1em" }}
+            >
+              <DatePicker />
+            </Form.Item>
+          </div>
+          <Button type="primary" htmlType="submit" style={{ marginBottom: 0 }}>
+            Submit
+          </Button>
         </Form>
       </Card>
     </article>
